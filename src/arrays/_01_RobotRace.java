6ae25a0b293f7bb26e800cd4d1550cba2608f45d
Joel Castro <Joel Castro@DESKTOP-OM0LJCA.mshome.net>
2019-02-15 21:40:39 -0800

    FindTheHiddenButton/RobotRace IP

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,22 +1,35 @@
 package arrays;
 
+import java.awt.Robot;
+import java.util.Random;
+
 public class _01_RobotRace {
 	//1. make a main method
-
+	public static void main(String[] args) {
+		
+	
 	//2. create an array of 5 robots.
-
+	Robot[] robots = new Robot[5];
 	//3. use a for loop to initialize the robots.
-
+	for (int i = 0; i<robots.length;i++)
+	{
+		//robots[i] = new Robot();
 		//4. make each robot start at the bottom of the screen, side by side, facing up
+		//robots[i]
+	}
 
-	//5. use another for loop to iterate through the array and make each robot move 
+	//5. use another for loop to iterate through the array and make each robot move
+	for (int i = 0; i<robots.length;i++) {
 	//   a random amount less than 50.
-
+		Random rand = new Random();
+		int thisMuch = rand.nextInt(51);
+		//robots[i].move(thisMuch);
+	}
 	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
-
 	//7. declare that robot the winner and throw it a party!
 
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
+	}
 }
